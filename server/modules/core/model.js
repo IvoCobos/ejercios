de1b513833927519d0513f82e6042d324acd935b
IvoCobos <ivancho3110@hotmail.com>
2022-06-10 11:31:01 -0500

    refactor limit and filter

--- server/modules/core/model.js
+++ server/modules/core/model.js
@@ -1,16 +1,5 @@
 export default (Model) => {
     return {
-        // find: async (filter) => {
-        //     const limit = filter.limit;
-        //     delete filter.limit;
-
-        //     const data = await Model.find(filter).limit(limit).lean().exec();
-            
-        //     return data;
-        // },
-        // update: async () =>{
-
-        // }
         async find(filter = {}){
             const limit = filter.limit;
             delete filter.limit;
