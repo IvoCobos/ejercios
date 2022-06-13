6c78fa74be7dfdf188f9035a817bb9ec4f258117
IvoCobos <ivancho3110@hotmail.com>
2022-06-13 11:27:29 -0500

    paginated

--- server/modules/core/model.js
+++ server/modules/core/model.js
@@ -1,16 +1,22 @@
 export default (Model) => {
     return {
-        async find(filter = {}){
+        async find(filter = {}) {
+            const page = filter.page;
+            delete filter.page;
+
             const limit = filter.limit;
             delete filter.limit;
 
             const mongooseInstance = Model.find(filter);
             mongooseInstance.limit(limit);
+            if (page) {
+                mongooseInstance.skip(page > 0 ? ((page - 1) * 10) : 0).limit(10);
+            }
             return mongooseInstance.exec();
         },
         async update() {
         },
-        async create(data = {}){
+        async create(data = {}) {
             const mongooseCreate = new Model(data);
             return mongooseCreate.save();
         }
