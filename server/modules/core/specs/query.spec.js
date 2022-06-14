1437a714f58b39ff11995536462808101f7e4d9e
IvoCobos <ivancho3110@hotmail.com>
2022-06-14 14:19:50 -0500

    test query

---
+++ server/modules/core/specs/query.spec.js
@@ -0,0 +1,23 @@
+import {expect, jest} from '@jest/globals';
+import defaultQuery from '../query';
+
+describe('', () => {
+  it('paginate', () => {
+    const mongooseQuery = {
+      skip: jest.fn().mockReturnThis(),
+      limit: jest.fn()
+    };
+    const query = defaultQuery(mongooseQuery);
+    query.paginate(5, 10);
+    expect(mongooseQuery.skip).toHaveBeenCalledWith(40);
+    expect(mongooseQuery.limit).toHaveBeenCalledWith(10);
+  });
+  it('populate', () => {
+    const mongooseQuery = {
+      populate: jest.fn()
+    };
+    const query = defaultQuery(mongooseQuery);
+    query.populate('user');
+    expect(mongooseQuery.populate).toHaveBeenCalledWith('user');
+  });
+});
